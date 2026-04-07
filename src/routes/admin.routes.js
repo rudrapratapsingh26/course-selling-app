e2797cb61c4805ed8366e7edd8d43b5ed48ac5ae
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-04-07 17:32:50 +0530

    feat(routes): add Admin routes for auth and course management

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { adminMiddleware } from "../middleware/admin.middleware.js";
+import {
+  registerAdmin,
+  loginAdmin,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+  getAdminCourses,
+} from "../controllers/admin.controllers.js";
+
+const router = Router();
+
+router.post("/signup", registerAdmin);
+router.post("/signin", loginAdmin);
+router
+  .route("/course")
+  .post(adminMiddleware, createCourse)
+  .get(adminMiddleware, getAdminCourses);
+router
+  .route("/course/:id")
+  .put(adminMiddleware, updateCourse)
+  .delete(adminMiddleware, deleteCourse);
+
+export default router;
