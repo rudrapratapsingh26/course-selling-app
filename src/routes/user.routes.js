6deb8b813b3ff3f07812a4c5a3d5e73aa6067185
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-04-07 17:31:36 +0530

    fix(routes): update user routes with correct imports and endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
+import { userMiddleware } from "../middleware/user.middleware.js";
 import {
   registerUser,
   loginUser,
   getPurchases,
 } from "../controllers/user.controllers.js";
-import { userMiddleware } from "../middleware/user.middlewares.js";
+
 const router = Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.post("/logout", verifyJWT, logoutUser);
-router.post("/refresh-token", refreshAccessToken);
+router.post("/signup", registerUser);
+router.post("/signin", loginUser);
+router.get("/purchases", userMiddleware, getPurchases);
 
 export default router;
