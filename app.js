1c2d9e3fc68346f9db19b83e886ee2f4c6a65e0b
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-04-08 11:22:43 +0530

    feat(app): configure express app with routers and global error handler

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { ApiResponse } from "./src/utils/api-response.js";
-import userRoutes from "./routes/userRoutes.js";
+import userRouter from "./src/routes/user.routes.js";
+import adminRouter from "./src/routes/admin.routes.js";
+import courseRouter from "./src/routes/course.routes.js";
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -15,3 +18,5 @@ app.use((err, req, res, next) => {
     .status(statusCode)
     .json(new ApiResponse(statusCode, null, message));
 });
+
+export default app;
