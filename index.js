c38e0abe3625b7d09d64dab98c04418e46f3fdc9
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-04-08 11:24:27 +0530

    chore(server): update index.js with dotenv config and DB connection

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-import app from "/app.js";
-import connectDB from "./src/database/database.js";
+import app from "./app.js";
+import connectDB from "./src/database/db.js";
 import dotenv from "dotenv";
-
-dotenv.config({
-    path: "./.env"
-});
+dotenv.config({ path: "./.env" });
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.on("error", (error) => {
-      console.log(`ERROR`, error);
-      throw error;
-    });
-
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server start running on port: ${process.env.PORT}`);
+      console.log(`Server running on port ${process.env.PORT}`);
     });
   } catch (error) {
-    console.log(`MongoDB connection failed`, error);
+    console.log("Server failed to start", error);
   }
 };
-
 
 startServer();
